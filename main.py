ebab92ab1e3833edc4b5640cf9e4b28cdd8dc14a
Mateusz Damian Chmurzyski <m.chmurzynski@stud.elka.pw.edu.pl>
2025-05-21 17:36:24 +0200

    P6 | actions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from q_learning import *
 
 def test_1():
-    q_learning(10,0.5,0.5)
+    q_learning(10,0.5,0.5, 10)
 
 
 if __name__ == '__main__':
--- q_learning.py
+++ q_learning.py
@@ -1,15 +1,33 @@
 import gym
 import numpy as np
 
-def q_learning(t_max, gamma, beta):
+def q_learning(t_max, gamma, beta, e_max):
     env = gym.make("Taxi-v3")
-    t = 0
+
     Q = np.zeros((env.observation_space.n, env.action_space.n))
-    state = env.reset()
-    while t < t_max:
-        action = action_choose(state)
-        next_observation, reward, terminated, truncated, info = env.step(action)
+    e = 0
+    while e < e_max:
+        t = 0
+        state = env.reset()
+        state = state[0]
+        print(state)
+        done = False
+        while t < t_max:
+            action = action_choose(state, Q)
+            print(action)
+            observation, reward, terminated, truncated, info = env.step(action)
+            print("Observation: ")
+            print(observation)
+            delta = reward + gamma * Q[observation][:] - Q[state][action]
+            Q = Q + beta * delta
+            t += 1
+            state = observation
+        e += 1
     return Q
 
-def action_choose(state):
-    return state.action_space.sample()
+def action_choose(state, Q):
+    if np.random.uniform() < 0.1:
+        action = np.argmax(Q[state])
+    else:
+        action = np.argmax(Q[state])
+    return int(action)
